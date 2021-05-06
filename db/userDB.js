73ec1131b9fafe3b6f584d24386f12596d54425c
gondoloAndreaCN <andrea.gondolo@itiscuneo.eu>
2021-05-06 12:38:19 +0200

    interface reg

---
+++ db/userDB.js
@@ -0,0 +1,8 @@
+const sqlite = require("sqlite3");
+const db = require("./db/connectionDB");
+
+module.exports = {
+    create(){
+        db.run(`INSERT INTO `)
+    }
+}
--- public/js/animation/anime.js
+++ public/js/animation/anime.js
@@ -18,21 +18,27 @@ anime({
 
 // Registrazione
 
-anime({
-  targets: '.decoration .el',
-  translateX: function (el) {
-    return  - el.getAttribute('data-x');
-  },
-  translateY: function(el, i) {
-    return 120 + (-100 * i);
-  },
-  scale: function(el, i, l) {
-    return (l - i) + .25;
-  },
-  rotate: function() { return anime.random(-360, 360); },
-  borderRadius: function() { return ['50%', anime.random(10, 35) + '%']; },
-  duration: function() { return anime.random(1200, 1800); },
-  delay: function() { return anime.random(0, 400); },
-  // direction: 'alternate',
-  // loop: true
-});
+
+
+if ($(window).width() > 700) {
+  anime({
+    targets: '.decoration .el',
+    translateX: function (el) {
+      return  - el.getAttribute('data-x');
+    },
+    translateY: function(el, i) {
+      return 120 + (-100 * i);
+    },
+    scale: function(el, i, l) {
+      return (l - i) + .25;
+    },
+    rotate: function() { return anime.random(-360, 360); },
+    borderRadius: function() { return ['50%', anime.random(10, 35) + '%']; },
+    duration: function() { return anime.random(1200, 1800); },
+    delay: function() { return anime.random(0, 400); },
+    // direction: 'alternate',
+    // loop: true
+  });
+} else {
+  // alert("alert")
+}

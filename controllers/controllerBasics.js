5810fd041e854a6aad6f9e1bae90fd61efd7730c
gondoloAndreaCN <andrea.gondolo@itiscuneo.eu>
2021-05-07 13:26:03 +0200

    alert reg

--- controllers/controllerBasics.js
+++ controllers/controllerBasics.js
@@ -9,40 +9,40 @@ exports.getRegistration = (req, res) => {
 
 exports.sendEmailReg = (req, res) => {
 
-    let name = req.body.Fname;
-    let surname = req.body.Lname;
+    // let name = req.body.Fname;
+    // let surname = req.body.Lname;
 
-    let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'scrapingproject5ainf@gmail.com',
-            pass: 'project2k21'
-        }
-    });
+    // let transporter = nodemailer.createTransport({
+    //     service: 'gmail',
+    //     auth: {
+    //         user: 'scrapingproject5ainf@gmail.com',
+    //         pass: 'project2k21'
+    //     }
+    // });
 
-    let mailOptions = {
-        from: 'scrapingproject5ainf@gmail.com',
-        to: req.body["email"],
-        subject: 'Avvenuta registrazione',
-        attachDataUrls: true,
-        html: '<h1> Thanks ' + `${name}  ${surname}` + '</h1> <br> <h2> Now you are part of the community </h2> <br>' +
-            '<img style="width: 600px" src="cid:unique@kreata.ee" alt="Photo">',
-        attachments: [
-            {
-                filename: 'event1.jpg',
-                path: 'public/img/event1.jpg',
-                cid: 'unique@kreata.ee'
-            }],
-    };
+    // let mailOptions = {
+    //     from: 'scrapingproject5ainf@gmail.com',
+    //     to: req.body["email"],
+    //     subject: 'Avvenuta registrazione',
+    //     attachDataUrls: true,
+    //     html: '<h1> Thanks ' + `${name}  ${surname}` + '</h1> <br> <h2> Now you are part of the community </h2> <br>' +
+    //         '<img style="width: 600px" src="cid:unique@kreata.ee" alt="Photo">',
+    //     attachments: [
+    //         {
+    //             filename: 'event1.jpg',
+    //             path: 'public/img/event1.jpg',
+    //             cid: 'unique@kreata.ee'
+    //         }],
+    // };
 
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    });
+    // transporter.sendMail(mailOptions, function (error, info) {
+    //     if (error) {
+    //         console.log(error);
+    //     } else {
+    //         console.log('Email sent: ' + info.response);
+    //     }
+    // });
 
-    console.log(req.body);
-    res.redirect("/");
+    // console.log(req.body);
+    res.redirect("/registration");
 }

8eb0a7639979ddf7c7b6c1729bfa19b118b44c16
gondoloAndreaCN <andrea.gondolo@itiscuneo.eu>
2021-05-13 11:23:56 +0200

    scraping

--- controllers/controllerBasics.js
+++ controllers/controllerBasics.js
@@ -1,26 +1,14 @@
 const nodemailer = require('nodemailer');
 const sqlite = require("sqlite3");
-const spawn = require("child_process").spawn;
+let {PythonShell} = require('python-shell')
 
 exports.getHome = (req, res) => {
     res.render("index", { title: "Home" });
 
-    var dataToSend;
-    // spawn new child process to call the python script
-    const python = spawn('python', ['../test.py']);
-    // collect data from script
-    python.stdout.on('data', data => {
-        console.log('Pipe data from python script ...');
-        dataToSend = data.toString();
-        // res.render("index", { title: "Home" });
-
-    });
-    // in close event we are sure that stream from child process is closed
-    python.on('close', (code) => {
-        console.log(`child process close all stdio with code ${code}`);
-        // send data to browser
-        
-    });
+    PythonShell.run('./scraping-event.py', null, function (err) {
+        if (err) throw err;
+        console.log('finished scraping');
+      });
 }
 
 exports.getRegistration = (req, res) => {
